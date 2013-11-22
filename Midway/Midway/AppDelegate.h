98bc1f58a50258444a89edf6f77e7e8dd8bf8923
Rostislav Raykov <z@zbrox.org>
2013-11-22 12:59:36 +0100

    Initial commit

---
+++ Midway/Midway/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Midway
+//
+//  Created by Rostislav Raykov on 11/22/13.
+//  Copyright (c) 2013 duckless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Midway/Midway/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Midway
+//
+//  Created by Rostislav Raykov on 11/22/13.
+//  Copyright (c) 2013 duckless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
