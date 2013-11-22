03098989e877a4dba5004e69f2d56015da647aab
Rostislav Raykov <z@zbrox.org>
2013-11-22 12:59:36 +0100

    Initial commit

---
+++ Midway/Midway/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Midway
+//
+//  Created by Rostislav Raykov on 11/22/13.
+//  Copyright (c) 2013 duckless. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+@end
