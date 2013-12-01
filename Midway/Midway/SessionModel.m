2d6ae741bbec62dc3162dedd37804cde874d5711
Olof Bjerke <olofbjerke@gmail.com>
2013-12-01 16:54:21 +0100

    Adding a Singleton object to handle session data, including emails, numbers and sessionIDs

---
+++ Midway/Midway/SessionModel.m
@@ -0,0 +1,82 @@
+//
+//  SessionModel.m
+//  Midway
+//
+//  Created by Olof Bjerke on 2013-12-01.
+//  Copyright (c) 2013 duckless. All rights reserved.
+//
+#import "AddressBookUI/AddressBookUI.h"
+#import "SessionModel.h"
+@interface SessionModel ()
+
+@property ABRecordID personID;
+@property NSMutableArray *emails;
+@property NSMutableArray *phoneNumbers;
+- (void) retrieveSessionID;
+- (void) gatherEmailAndPhoneNumbers;
+
+@end
+@implementation SessionModel
+
+-(id) init {
+    self = [super init];
+    if(self) {
+        _emails = [[NSMutableArray alloc] init];
+        _phoneNumbers = [[NSMutableArray alloc] init];
+//        [self loadData];
+        
+    }
+    return self;
+}
+
++ (id)sharedSessionModel {
+    static SessionModel *sharedSessionModel = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedSessionModel = [[self alloc] init];
+    });
+    return sharedSessionModel;
+}
+
+- (NSMutableArray *) inviteesEmails {
+    return self.emails;
+}
+
+- (NSMutableArray *) inviteesPhoneNumbers {
+    return self.phoneNumbers;
+}
+
+- (void) startSessionWith:(ABRecordID)invitee {
+    self.personID = invitee;
+    
+    [self gatherEmailAndPhoneNumbers];
+    NSLog(@"got an ID!");
+    
+}
+
+- (void) gatherEmailAndPhoneNumbers {
+    
+    NSLog(@"Gather email and phone info");
+    [self.emails removeAllObjects];
+    [self.phoneNumbers removeAllObjects];
+    
+    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(nil, nil);
+    ABRecordRef personRef = ABAddressBookGetPersonWithRecordID(addressBook, self.personID);
+    
+    ABMultiValueRef emailMultiValue = ABRecordCopyValue(personRef, kABPersonEmailProperty);
+    NSArray *emailAddresses = (__bridge NSArray *)ABMultiValueCopyArrayOfAllValues(emailMultiValue);
+    
+    [self.emails addObjectsFromArray:emailAddresses];
+    
+    ABMultiValueRef phoneMultiValue = ABRecordCopyValue(personRef, kABPersonPhoneProperty);
+    NSArray *phoneNumbers = (__bridge NSArray *)ABMultiValueCopyArrayOfAllValues(phoneMultiValue);
+
+    [self.phoneNumbers addObjectsFromArray:phoneNumbers];
+    
+}
+- (void) retrieveSessionID {
+    // This message should contact the server in the
+    // background, retrieving a new session ID to be used when an email or SMS is sent
+}
+
+@end
