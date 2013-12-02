b43528a960deb31803c51e20cf717451485d33d7
Olof Bjerke <olofbjerke@gmail.com>
2013-12-02 10:58:36 +0100

    Removed one view from storyboard. InviteMethods now handle email and SMS.

--- Midway/Midway/InviteMethodsViewController.h
+++ Midway/Midway/InviteMethodsViewController.h
@@ -6,10 +6,13 @@
 //  Copyright (c) 2013 duckless. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
 #import "AddressBookUI/AddressBookUI.h"
+#import <MessageUI/MFMailComposeViewController.h>
+#import "MessageUI/MFMessageComposeViewController.h"
 
-@interface InviteMethodsViewController : UITableViewController
+@interface InviteMethodsViewController : UITableViewController <MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate>
 
 @property ABRecordID personID;
 
