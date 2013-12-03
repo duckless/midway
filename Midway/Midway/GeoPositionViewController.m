3f089a52c37523102dddfc32d09b00bcb25b3954
Olof Bjerke <olofbjerke@gmail.com>
2013-12-03 11:05:37 +0100

    Added a red border and a green background view for the compass. Still not rotating around its center :/

--- Midway/Midway/GeoPositionViewController.m
+++ Midway/Midway/GeoPositionViewController.m
@@ -5,7 +5,7 @@
 //  Created by Olof Bjerke on 2013-12-02.
 //  Copyright (c) 2013 duckless. All rights reserved.
 //
-
+#import "SessionModel.h"
 #import "GeoPositionViewController.h"
 
 @interface GeoPositionViewController ()
@@ -44,7 +44,7 @@
 }
 
 
-
+/*
 - (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
 {
     // Convert Degree to Radian and move the needle
@@ -58,6 +58,48 @@
     [self.geoArrow.layer addAnimation:theAnimation forKey:@"animateMyRotation"];
     self.geoArrow.transform = CGAffineTransformMakeRotation(newRad);	NSLog(@"%f (%f) => %f (%f)", manager.heading.trueHeading, oldRad, newHeading.trueHeading, newRad);
 }
+ 
+ double lon = location.longitude - otherLocation.longitude;
+ double y = sin(lon) * cos(otherLocation.latitude);
+ double x = cos(location.latitude) * sin(otherLocation.latitude) - sin(location.latitude) * cos(otherLocation.latitude) * cos(lon);
+ double angle = atan2(y, x);
+ 
+*/
 
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading{
+    
+    CLLocation *location = manager.location;
+    CLLocation *otherLocation = [[SessionModel sharedSessionModel] targetLocation];
+    
+    double distanceEast = (location.coordinate.longitude > 0 && otherLocation.coordinate.longitude < 0) ? 180 - location.coordinate.longitude + otherLocation.coordinate.longitude - -180: otherLocation.coordinate.longitude - location.coordinate.longitude;
+    
+    if (distanceEast < 0) {
+        distanceEast += 360;
+    }
+    
+    double distanceWest = (location.coordinate.longitude < 0 && otherLocation.coordinate.longitude > 0) ? -180 - location.coordinate.longitude - 180 - otherLocation.coordinate.longitude : location.coordinate.longitude - otherLocation.coordinate.longitude;
+    
+    if (distanceWest < 0) {
+        distanceWest += 360;
+    }
+    
+    float latitudinalDifference = (otherLocation.coordinate.latitude - location.coordinate.latitude);
+    float longitudinalDifference = fmin(distanceEast,distanceWest);
+    
+    float arcTan = atan(longitudinalDifference / latitudinalDifference);
+    
+    float oldRadian = (-manager.heading.trueHeading *M_PI /180.0f)+arcTan+M_PI;
+    float newRadian = (-newHeading.trueHeading *M_PI /180.0f)+arcTan+M_PI;
+    
+    CABasicAnimation *animation;
+    animation=[CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+    animation.fromValue = [NSNumber numberWithFloat:oldRadian];
+    animation.toValue = [NSNumber numberWithFloat:newRadian];
+    //animation.duration = 0.1f;
+    self.geoArrow.layer.anchorPoint = CGPointMake(0.5, 0.5);
+    
+//    [self.geoArrow.layer addAnimation:animation forKey:nil];
+    self.geoArrow.transform = CGAffineTransformMakeRotation(newRadian);
+}
 
 @end
