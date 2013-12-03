e77c41eb67a18ab4fe5cd7199692134a69034df9
Rostislav Raykov <z@zbrox.org>
2013-12-03 15:54:56 +0100

    Joining a session - no checks for multiple people in session or unique uuids per session;

--- midway-server/app/controllers/session_controller.rb
+++ midway-server/app/controllers/session_controller.rb
@@ -14,9 +14,27 @@ class SessionController < ApplicationController
   end
 
   def join
-    
+    @session = Session.find_by_session_id(params[:session_id])
+    if @session.nil?
+      render json: [{:error => "Invalid session"}]
+      return
+    end
+
+    begin
+      @participant = Participant.create!({:uuid => params[:uuid], :last_location => params[:last_location]})
+    rescue
+      render json: [{:error => "Insufficient parameters"}]
+      return
+    end
+
+    @other_participant = Participant.find_by_session_id(@session.id)
+    @session.participants << @participant
+
+    render json: [{:session_id => @session.session_id, :location => @other_participant.last_location}]
   end
 
   def midpoint
+
   end
+
 end
