48c2493b9c80dbe2f698e8c95b74cbe397e6e3f2
Olof Bjerke <olofbjerke@gmail.com>
2013-12-06 12:26:10 +0100

    CompassView now receives notification with embedded heading. Location manager now uses the session model because the locationmanager sends the notification with the heading to the view.

--- Midway/Midway/CompassView.m
+++ Midway/Midway/CompassView.m
@@ -8,12 +8,11 @@
 
 #import "CompassView.h"
 #import "SessionModel.h"
-#define degreesToRadians(x) (M_PI * x / 180.0)
-#define radiansToDegrees(x) (x * 180 / M_PI)
+
 
 @interface CompassView()
 
-- (void) updateCompass;
+- (void) updateCompassWithHeading: (double) compassHeading;
 
 @end
 
@@ -24,104 +23,41 @@
     self = [super initWithFrame:frame];
     if (self)
     {
-        
-//        Compass Images
+    
         UIImageView *arrowImg = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
         arrowImg.image = [UIImage imageWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"golden_arrow" ofType:@"png"]];
-        
-//        Compass Container
+    
         self.compassContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
-        
-//        [self.compassContainer.layer  insertSublayer:myCompassLayer atIndex:0];
+    
         [self.compassContainer addSubview:arrowImg];
         
         [self addSubview:self.compassContainer];
         
-        self.locationManager = [[CLLocationManager alloc] init];
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-        self.locationManager.headingFilter = 1;
-        self.locationManager.distanceFilter = kCLDistanceFilterNone;
-        self.locationManager.delegate=self;
-        
-//        Start the compass updates.
-        [self.locationManager startUpdatingHeading];
-        [self.locationManager startUpdatingLocation];
-        
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(stopUpdating) name:UIApplicationDidEnterBackgroundNotification object:nil];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(startUpdating) name:UIApplicationDidBecomeActiveNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveNotification:) name:@"updateCompass" object:nil];
         
     }
     return self;
 }
 
-- (void) startUpdating
+- (void) receiveNotification:(NSNotification *)notification
 {
-    NSLog(@"starting");
-    
-    [self.locationManager startUpdatingHeading];
+    NSLog(@"get notification in view");
+    if ([notification.name isEqualToString:@"updateCompass"])
+    {
+        NSDictionary* userInfo = notification.userInfo;
+        double heading = [[userInfo objectForKey:@"heading"] doubleValue];
+       
+        [self updateCompassWithHeading: heading];
+    }
 }
 
-- (void) stopUpdating
+- (void) updateCompassWithHeading: (double) compassHeading
 {
-    NSLog(@"Stopping");
-    
-    [self.locationManager stopUpdatingHeading];
-}
 
-/*
- // Only override drawRect: if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- - (void)drawRect:(CGRect)rect
- {
- // Drawing code
- }
- */
-
-#pragma mark -
-#pragma mark Geo Points methods
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
-{
-	[self updateCompass];
-}
-
-- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
-{
-    [self updateCompass];
-}
-
-- (void) updateCompass
-{
-    NSInteger trueAngle = self.locationManager.heading.trueHeading;
-    CLLocation * targetLocation = [[SessionModel sharedSessionModel] targetLocation];
+//    self.coordinates.text = [[NSString alloc] initWithFormat:@"lat: %f, lon: %f", lat1, lon1];
+//    self.distance.text = [[NSString alloc] initWithFormat:@"%f meter left", distance];
     
-    // Current location
-    float lat1 = self.locationManager.location.coordinate.latitude;
-    float lon1 = self.locationManager.location.coordinate.longitude;
-    
-    // Target location
-    float lat2 = targetLocation.coordinate.latitude;
-    float lon2 = targetLocation.coordinate.longitude;
-    
-    // Distance between coordinates
-    float distance = [self.locationManager.location distanceFromLocation: targetLocation];
-    
-    float headingToTarget = atan2(sin(lon2 - lon1) * cos(lat2), cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(lon2 - lon1));
-    float headingInDegrees = radiansToDegrees(headingToTarget);
-    
-    
-    
-    float compassHeading = headingInDegrees - trueAngle;
-    
-    if (compassHeading < 0)
-        compassHeading = compassHeading + 360;
-    
-    NSLog(@"compassHeading: %f", compassHeading);
-    
-    self.coordinates.text = [[NSString alloc] initWithFormat:@"lat: %f, lon: %f", lat1, lon1];
-    self.distance.text = [[NSString alloc] initWithFormat:@"%f meter left", distance];
-    
-    CGAffineTransform rotate = CGAffineTransformMakeRotation(degreesToRadians(compassHeading));
+    CGAffineTransform rotate = CGAffineTransformMakeRotation(compassHeading);
     [self.compassContainer setTransform:rotate];
 }
 
