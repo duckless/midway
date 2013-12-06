3b37739d27ba1f9c00503e57edadfb9f7edf0a5b
Rostislav Raykov <z@zbrox.org>
2013-12-06 16:41:49 +0100

    Add a sample foursquare config; Add foursquare gem; Find middle point of two GPS coords; Find the nearest cafe given a GPS lat/lng pair; Return nearest cafe on session join;

--- midway-server/app/controllers/session_controller.rb
+++ midway-server/app/controllers/session_controller.rb
@@ -30,11 +30,34 @@ class SessionController < ApplicationController
     @other_participant = Participant.find_by_session_id(@session.id)
     @session.participants << @participant
 
-    render json: [{:session_id => @session.session_id, :location => @other_participant.last_location}]
+    midway_fika = find_fika middle_pos(@other_participant.last_location,@participant.last_location)
+
+    render json: [{:session_id => @session.session_id, :location => midway_fika}]
   end
 
   def midpoint
 
   end
+
+  protected
+    def middle_pos(location1, location2)
+      location1 = location1.split(",").map {|i| Float(i.strip)}
+      location2 = location2.split(",").map {|i| Float(i.strip)}
+
+      mid_point = "#{(location1[0]+location2[0])/2},#{(location1[1]+location2[1])/2}"
+    end
+
+    def find_fika(location)
+      path = File.join(Rails.root, "config", "foursquare.yml")
+      config = YAML.load_file(path)
+
+      client = Foursquare2::Client.new(:client_id => config['foursquare']['client_id'], 
+        :client_secret => config['foursquare']['client_secret'])
+
+      venues = client.search_venues(:ll => location, 
+        :categoryId => config['foursquare']['cafe_category_id'])
+      location = venues['groups'][0]['items'][0]['location']
+      "#{location[:lat]},#{location[:lng]}"
+    end
 
 end
