0f3cef5a07872a8cd866c9485735072106ea1cca
kosmopolska <anton.malmquist@me.com>
2013-12-06 11:09:47 +0100

    created header for location manager

---
+++ Midway/Midway/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  Midway
+//
+//  Created by Anton Malmquist on 2013-12-06.
+//  Copyright (c) 2013 duckless. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
