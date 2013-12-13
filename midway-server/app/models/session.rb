801d53c0adcf5b1d6f43eea93c74bb8461e8e085
Rostislav Raykov <z@zbrox.org>
2013-12-13 16:10:26 +0100

    Shorten the session ID;

--- midway-server/app/models/session.rb
+++ midway-server/app/models/session.rb
@@ -8,7 +8,7 @@ class Session < ActiveRecord::Base
 
     def generate_unique_session_id
       self.session_id = loop do
-        random_session_id = SecureRandom.urlsafe_base64(nil, false)
+        random_session_id = SecureRandom.urlsafe_base64(7, false)
         break random_session_id unless Session.exists?(session_id: random_session_id)
       end
     end
